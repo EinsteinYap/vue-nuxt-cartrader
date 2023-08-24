817658e26bc2a3e4bd37eb533dd304b407797a9c
EinsteinYap <einsteinyap2014@gmail.com>
2023-08-24 23:39:15 +0800

    added favorite car make, car price range installed some package

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,11 +1,11 @@
 <template>
   <div
-    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
-    @click="navigateTo(`/car/${car.name}-${car.id}`)"
+    class="relative shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+    
   >
-    <div class="flex h-full">
-      <img :src="car.url" alt="" class="w-[300px] h-full" />
-      <p>{{ car.id }} , {{ car.name }}</p>
+  <img class="absolute w-7 right-5 top-2 z-20" :src="favored ? heartFilled: heartOutline" alt="" @click="emit('favor',car.id)">
+    <div class="flex h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
+      <NuxtImg :src="car.url" alt="" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
           <h1 class="text-2xl text-blue-700">
@@ -25,10 +25,17 @@ const props =defineProps({
   car:Object,
 })
 </script> -->
-<script>
-export default {
-  props: {
-    car: Object,
-  },
-};
+<script setup>
+import heartFilled from "@/assets/heartFilled.png";
+import heartOutline from "@/assets/heartOutline.png";
+const props =defineProps({
+  car:Object,
+  favored:Boolean
+})
+const emit =defineEmits(['favor'])
+// const favored = useState(`favored-${props.car.id}`,()=>{
+//     return false
+//   });
+
+
 </script>
